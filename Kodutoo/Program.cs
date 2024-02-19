c855a211393566f5eb5da6f29231c09a84a35e12
JohannesG <johannes.gevlja@tktk.ee>
2024-02-20 00:38:33 +0200

    add week3 code

---
+++ Kodutoo/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Mis hinde kasutaja sai?");
+
+char userGrade = char.Parse(Console.ReadLine());
+string userGrade = Console.ReadLine();
+
+
+if (userGrade == A) ;
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (userGrade == B);
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (userGrade == C) ;
+{
+    Console.WriteLine("Hea!");
+}
+else if (userGrade == D) ;
+{
+    Console.WriteLine("Rahuldav!");
+}
+else if (userGrade == E) ;
+{
+    Console.WriteLine("Kasin!");
+}
+else if (userGrade == F) ;
+{
+    Console.WriteLine("Puudulik!");
+}
+
+else
+{
+    Console.WriteLine("Vale väärtus.");
+}
